89d584f78f16b130e7b10a424ac0af1b3869dca0
lioudong <lod@keepyoga.com>
2018-05-28 13:49:23 +0800

    readme调整

--- README.md
+++ README.md
@@ -1,37 +1,32 @@
 Pagination
 ==========
 
-jquery.pagination.js 的修改版,增加了一些参数,改变了回调处理方式
-
-具体修改
-
-1:新增参数`trigger`,只有当`trigger`为`true`的时候,才会执行回调;
-
-2:新增首页末页设置(`first_text`,`end_text`,`first_show_always`,`end_show_always`);
-
-3:修改`link_to`属性的`__id__`值与url一致(从1开始);
-
 基本使用:
 ```
-$("#Pagination").pagination(<%=num%>, //总条目数;必选参数，整数
-{
-  items_per_page:2,//每页显示的条目数;可选参数，默认是10
-  num_display_entries:8,//连续分页主体部分显示的分页条目数;可选参数，默认是10
-  num_edge_entries:1,//两侧显示的首尾分页的条目数;可选参数,默认0
-  first_text:'首页',//首页显示文字;可选字符串参数,默认是 'Home'
-  end_text:'末页', //末页显示文字;可选字符串参数,默认是 'End'
-  prev_text:'上一页',//“前一页”分页按钮上显示的文字;  可选字符串参数，默认是"Prev"
-  next_text:'下一页',//“下一页”分页按钮上显示的文字;  可选字符串参数，默认是"Next"
-  link_to:'/index/list/page/__id__',//分页的链接;字符串，可选参数，默认是"#"
-  current_page:<%=page-1%>,//当前页码;可选参数,默认是1
-  ellipse_text:'...',//	省略的页码;可选参数,默认是'...'
-  prev_show_always:true,//是否显示“前一页”分页按钮;可选参数,默认是 true
-  next_show_always:true,//是否显示"下一页"分页按钮;可选参数,默认是true
-  trigger:false,//是否触发回调
-  callback:null //回调方法;接受两个参数，新一页的id和pagination容器(一个DOM元素),如果回调函数返回false，则pagination事件停止执行
-});
-<% %>区块为动态数据
+$("#Pagination").pagination(total, options);
 ```
 
-//下面地址已不可用~~
-详细API参考:[http://tutorials.ajaxmasters.com/pagination-demo/](http://tutorials.ajaxmasters.com/pagination-demo/)
+参数说明：
+```
+
+| 参数名称    | 参数数据类型   |  默认值  |  是否必填  |  描述  |
+| --------   | -----:       | :----:  | :----:    | :----: |
+| total     | Number |     null   | true | 数据总量 |
+| options.items_per_page      |   Number   |   10  | false | 每页显示的条目数 |
+| options.num_display_entries  |  Number |  10  | false | 连续分页主体部分显示的分页条目数 |
+| options.current_page | Number | 0 | false | 当前页码 |
+| options.num_edge_entries | Number | 0 | false | 两侧显示的首尾分页的条目数 |
+| options.link_to | String | '#' | false | 分页的链接 |
+| options.prev_text | String | 'Prev' | false | “前一页”分页按钮上显示的内容 |
+| options.next_text | String | 'Next' | false | “下一页”分页按钮上显示的内容 |
+| options.first_text | String | 'Home' | false | “首页”显示文字 |
+| options.end_text | String | 'End' | false | “末页”显示文字 |
+| options.ellipse_text | String | '...' | false | 省略的页码 |
+| options.prev_show_always | Boolean | true | false | 是否显示“前一页”分页按钮 |
+| options.next_show_always | Boolean | true | false | 是否显示"下一页"分页按钮 |
+| options.first_show_always | Boolean | true | false | 是否显示“首页”分页按钮 |
+| options.end_show_always | Boolean | true |  false | 是否显示"末页"分页按钮  |
+| options.trigger | Boolean | true | false | 是否触发回调 |
+| options.callback | Function | function | false | 回调方法;接受两个参数，新一页的id和pagination容器(一个DOM元素),如果回调函数返回false，则pagination事件停止执行 |
+
+```
--- jquery.pagination.js
+++ jquery.pagination.js
@@ -6,69 +6,93 @@
  * @param {int} maxentries Number of entries to paginate
  * @param {Object} opts Several options (see README for documentation)
  * @return {Object} jQuery Object
- * @update 新增参数trigger,只有当trigger为true的时候,才会执行回调;新增首页末页设置;修改link_to的__id__值与url一致(从1开始);
+ * /
+ 
+ /*
+ * 原插件资源已不可用，当前版本为修改版
+ * 1:新增参数`trigger`,只有当`trigger`为`true`的时候,才会执行回调;
+ * 2:新增首页末页设置(`first_text`,`end_text`,`first_show_always`,`end_show_always`); 
+ * 3:修改`link_to`属性的`__id__`值与url一致(从1开始);
+ * @website https://github.com/tabooc/Pagination
  */
-jQuery.fn.pagination = function(maxentries, opts){
+jQuery.fn.pagination = function(maxentries, opts) {
 	opts = jQuery.extend({
-		items_per_page:10,
-		num_display_entries:10,
-		current_page:0,
-		num_edge_entries:0,
-		link_to:"#",
-		prev_text:"Prev",
-		next_text:"Next",
-		first_text:"Home",
-		end_text:"End",
-		ellipse_text:"...",
-		prev_show_always:true,
-		next_show_always:true,
-		first_show_always:true,
-		end_show_always:true,
-		trigger:true,
-		callback:function(){return false;}
-	},opts||{});
-	
+		//每页显示的条目数;可选参数，默认是10
+		items_per_page: 10,
+		//连续分页主体部分显示的分页条目数;可选参数，默认是10
+		num_display_entries: 10,
+		//当前页码
+		current_page: 0, 
+		//两侧显示的首尾分页的条目数;可选参数,默认0
+		num_edge_entries: 0,
+		//分页的链接;字符串，可选参数
+		link_to: "#",
+		//“前一页”分页按钮上显示的文字;  可选字符串参数
+		prev_text: "Prev",
+		//“下一页”分页按钮上显示的文字;  可选字符串参数
+		next_text: "Next",
+		//首页显示文字;可选字符串参数
+		first_text: "Home",
+		//末页显示文字;可选字符串参数
+		end_text: "End",
+		//省略的页码;可选参数
+		ellipse_text: "...",
+		//是否显示“前一页”分页按钮;可选参数
+		prev_show_always: true,
+		//是否显示"下一页"分页按钮;可选参数
+		next_show_always: true,
+		//是否显示“首页”分页按钮;可选参数
+		first_show_always: true,
+		//是否显示"末页"分页按钮;可选参数
+		end_show_always: true,
+		//是否触发回调
+		trigger: true,
+		//回调方法;接受两个参数，新一页的id和pagination容器(一个DOM元素),如果回调函数返回false，则pagination事件停止执行
+		callback: function() { 
+			return false;
+		}
+	}, opts || {});
+
 	return this.each(function() {
 		/**
 		 * Calculate the maximum number of pages
 		 */
 		function numPages() {
-			return Math.ceil(maxentries/opts.items_per_page);
+			return Math.ceil(maxentries / opts.items_per_page);
 		}
-		
+
 		/**
 		 * Calculate start and end point of pagination links depending on 
 		 * current_page and num_display_entries.
 		 * @return {Array}
 		 */
-		function getInterval()  {
-			var ne_half = Math.ceil(opts.num_display_entries/2);
+		function getInterval() {
+			var ne_half = Math.ceil(opts.num_display_entries / 2);
 			var np = numPages();
-			var upper_limit = np-opts.num_display_entries;
-			var start = current_page>ne_half?Math.max(Math.min(current_page-ne_half, upper_limit), 0):0;
-			var end = current_page>ne_half?Math.min(current_page+ne_half, np):Math.min(opts.num_display_entries, np);
-			return [start,end];
+			var upper_limit = np - opts.num_display_entries;
+			var start = current_page > ne_half ? Math.max(Math.min(current_page - ne_half, upper_limit), 0) : 0;
+			var end = current_page > ne_half ? Math.min(current_page + ne_half, np) : Math.min(opts.num_display_entries, np);
+			return [start, end];
 		}
-		
+
 		/**
 		 * This is the event handling function for the pagination links. 
 		 * @param {int} page_id The new page number
 		 */
-		function pageSelected(page_id, evt){
+		function pageSelected(page_id, evt) {
 			current_page = page_id;
 			drawLinks();
 			var continuePropagation = opts.callback(page_id, panel);
 			if (!continuePropagation) {
 				if (evt.stopPropagation) {
 					evt.stopPropagation();
-				}
-				else {
+				} else {
 					evt.cancelBubble = true;
 				}
 			}
 			return continuePropagation;
 		}
-		
+
 		/**
 		 * This function inserts the pagination links into the container element
 		 */
@@ -78,101 +102,114 @@ jQuery.fn.pagination = function(maxentries, opts){
 			var np = numPages();
 			// This helper function returns a handler function that calls pageSelected with the right page_id
 			var getClickHandler = function(page_id) {
-				return function(evt){ return pageSelected(page_id,evt); }
+				return function(evt) {
+					return pageSelected(page_id, evt);
+				}
 			}
 			// Helper function for generating a single link (or a span tag if it's the current page)
-			var appendItem = function(page_id, appendopts){
-				page_id = page_id<0?0:(page_id<np?page_id:np-1); // Normalize page id to sane value
-				appendopts = jQuery.extend({text:page_id+1, classes:""}, appendopts||{});
-				if(page_id == current_page){
-					var lnk = jQuery("<span class='current'>"+(appendopts.text)+"</span>");
-				}
-				else
-				{
-					var lnk = jQuery("<a>"+(appendopts.text)+"</a>")						
-						.attr('href', opts.link_to.replace(/__id__/,page_id+1));
+			var appendItem = function(page_id, appendopts) {
+				page_id = page_id < 0 ? 0 : (page_id < np ? page_id : np - 1); // Normalize page id to sane value
+				appendopts = jQuery.extend({
+					text: page_id + 1,
+					classes: ""
+				}, appendopts || {});
+				if (page_id == current_page) {
+					var lnk = jQuery("<span class='current'>" + (appendopts.text) + "</span>");
+				} else {
+					var lnk = jQuery("<a>" + (appendopts.text) + "</a>")
+						.attr('href', opts.link_to.replace(/__id__/, page_id + 1));
 
-						if(opts.trigger){
-							lnk.bind("click", getClickHandler(page_id));
-						}
-						
-						
+					if (opts.trigger) {
+						lnk.bind("click", getClickHandler(page_id));
+					}
+
+
 				}
-				if(appendopts.classes){lnk.addClass(appendopts.classes);}
+				if (appendopts.classes) {
+					lnk.addClass(appendopts.classes);
+				}
 				panel.append(lnk);
 			}
 			// Generate "first"-Link
-			if(opts.first_text && (current_page > 1 || opts.first_show_always)){
-				appendItem(0,{text:opts.first_text, classes:"first"});
+			if (opts.first_text && (current_page > 1 || opts.first_show_always)) {
+				appendItem(0, {
+					text: opts.first_text,
+					classes: "first"
+				});
 			}
 			// Generate "Previous"-Link
-			if(opts.prev_text && (current_page > 0 || opts.prev_show_always)){				
-				appendItem(current_page-1,{text:opts.prev_text, classes:"prev"});
+			if (opts.prev_text && (current_page > 0 || opts.prev_show_always)) {
+				appendItem(current_page - 1, {
+					text: opts.prev_text,
+					classes: "prev"
+				});
 			}
 
 			// Generate starting points
-			if (interval[0] > 0 && opts.num_edge_entries > 0)
-			{
+			if (interval[0] > 0 && opts.num_edge_entries > 0) {
 				var end = Math.min(opts.num_edge_entries, interval[0]);
-				for(var i=0; i<end; i++) {
+				for (var i = 0; i < end; i++) {
 					appendItem(i);
 				}
-				if(opts.num_edge_entries < interval[0] && opts.ellipse_text)
-				{
-					jQuery("<span>"+opts.ellipse_text+"</span>").appendTo(panel);
+				if (opts.num_edge_entries < interval[0] && opts.ellipse_text) {
+					jQuery("<span>" + opts.ellipse_text + "</span>").appendTo(panel);
 				}
 			}
 			// Generate interval links
-			for(var i=interval[0]; i<interval[1]; i++) {
+			for (var i = interval[0]; i < interval[1]; i++) {
 				appendItem(i);
 			}
 			// Generate ending points
-			if (interval[1] < np && opts.num_edge_entries > 0)
-			{
-				if(np-opts.num_edge_entries > interval[1]&& opts.ellipse_text)
-				{
-					jQuery("<span>"+opts.ellipse_text+"</span>").appendTo(panel);
+			if (interval[1] < np && opts.num_edge_entries > 0) {
+				if (np - opts.num_edge_entries > interval[1] && opts.ellipse_text) {
+					jQuery("<span>" + opts.ellipse_text + "</span>").appendTo(panel);
 				}
-				var begin = Math.max(np-opts.num_edge_entries, interval[1]);
-				for(var i=begin; i<np; i++) {
+				var begin = Math.max(np - opts.num_edge_entries, interval[1]);
+				for (var i = begin; i < np; i++) {
 					appendItem(i);
 				}
-				
+
 			}
 			// Generate "Next"-Link
-			if(opts.next_text && (current_page < np-1 || opts.next_show_always)){
-				appendItem(current_page+1,{text:opts.next_text, classes:"next"});
+			if (opts.next_text && (current_page < np - 1 || opts.next_show_always)) {
+				appendItem(current_page + 1, {
+					text: opts.next_text,
+					classes: "next"
+				});
 			}
 			// Generate "End"-Link
-			if(opts.end_text && (current_page < np-1 || opts.end_show_always)){
-				appendItem(np,{text:opts.end_text, classes:"end"});
+			if (opts.end_text && (current_page < np - 1 || opts.end_show_always)) {
+				appendItem(np, {
+					text: opts.end_text,
+					classes: "end"
+				});
 			}
 		}
-		
+
 		// Extract current_page from options
 		var current_page = opts.current_page;
 		// Create a sane value for maxentries and items_per_page
-		maxentries = (!maxentries || maxentries < 0)?1:maxentries;
-		opts.items_per_page = (!opts.items_per_page || opts.items_per_page < 0)?1:opts.items_per_page;
+		maxentries = (!maxentries || maxentries < 0) ? 1 : maxentries;
+		opts.items_per_page = (!opts.items_per_page || opts.items_per_page < 0) ? 1 : opts.items_per_page;
 		// Store DOM element for easy access from all inner functions
 		var panel = jQuery(this);
 		// Attach control functions to the DOM element 
-		this.selectPage = function(page_id){ pageSelected(page_id);}
-		this.prevPage = function(){ 
+		this.selectPage = function(page_id) {
+			pageSelected(page_id);
+		}
+		this.prevPage = function() {
 			if (current_page > 0) {
 				pageSelected(current_page - 1);
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
 		}
-		this.nextPage = function(){ 
-			if(current_page < numPages()-1) {
-				pageSelected(current_page+1);
+		this.nextPage = function() {
+			if (current_page < numPages() - 1) {
+				pageSelected(current_page + 1);
 				return true;
-			}
-			else {
+			} else {
 				return false;
 			}
 		}
